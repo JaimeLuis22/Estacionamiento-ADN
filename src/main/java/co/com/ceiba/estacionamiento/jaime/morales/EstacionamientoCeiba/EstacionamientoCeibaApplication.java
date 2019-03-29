af99ab2c9af42f8d34c757f071c3f7d23e17c4b9
Jaime Luis Morales Guzmán - Ceiba Software <jaime.morales@ceiba.com.co>
2019-03-29 14:01:34 -0500

    Inicializacion del proyecto ADN

---
+++ src/main/java/co/com/ceiba/estacionamiento/jaime/morales/EstacionamientoCeiba/EstacionamientoCeibaApplication.java
@@ -0,0 +1,13 @@
+package co.com.ceiba.estacionamiento.jaime.morales.EstacionamientoCeiba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacionamientoCeibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstacionamientoCeibaApplication.class, args);
+	}
+
+}
